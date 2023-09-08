adeed1c6b73425b45d3bd28ef05198e83a476dba
NituAlexandru <38262850+NituAlexandru@users.noreply.github.com>
2023-09-08 19:06:59 +0000

    Deploying to gh-pages from @ robertSovar/simply-chocolate-project@5f58a8458659646543543967f787a2d863d9260a 🚀

---
+++ index.8313adf7.js
@@ -0,0 +1,2 @@
+!function(){var e={openModalBtn:document.querySelector("[data-modal-open-header]"),closeModalBtn:document.querySelector("[data-modal-close-header]"),modal:document.querySelector("[data-modal-header]")};function d(){e.modal.classList.toggle("header-modal-is-hidden")}e.openModalBtn.addEventListener("click",d),e.closeModalBtn.addEventListener("click",d)}();
+//# sourceMappingURL=index.8313adf7.js.map
---
+++ index.b1e2b241.js
@@ -0,0 +1,2 @@
+(()=>{const e={openModalBtn:document.querySelector("[data-modal-open-header]"),closeModalBtn:document.querySelector("[data-modal-close-header]"),modal:document.querySelector("[data-modal-header]")};function d(){e.modal.classList.toggle("header-modal-is-hidden")}e.openModalBtn.addEventListener("click",d),e.closeModalBtn.addEventListener("click",d)})();
+//# sourceMappingURL=index.b1e2b241.js.map
