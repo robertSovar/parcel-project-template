1249958d3ff6eab37c3c6248dca38143e3288bf6
roberSovar <sovar_robert_ionut@yahoo.com>
2023-11-05 17:52:28 +0200

    update hero-branch
    Just a simple commit to have my changes on my laptop

--- src/index.js
+++ src/index.js
@@ -1,15 +1,32 @@
- //  Header modal
+//  Header modal
 (() => {
   const headerModal = {
-    openModalBtn: document.querySelector("[data-modal-open-header]"),
-    closeModalBtn: document.querySelector("[data-modal-close-header]"),
-    modal: document.querySelector("[data-modal-header]"),
+    openModalBtn: document.querySelector('[data-modal-open-header]'),
+    closeModalBtn: document.querySelector('[data-modal-close-header]'),
+    modal: document.querySelector('[data-modal-header]'),
   };
 
-  headerModal.openModalBtn.addEventListener("click", toggleModal);
-  headerModal.closeModalBtn.addEventListener("click", toggleModal);
+  headerModal.openModalBtn.addEventListener('click', toggleModal);
+  headerModal.closeModalBtn.addEventListener('click', toggleModal);
 
   function toggleModal() {
-    headerModal.modal.classList.toggle("header-modal-is-hidden");
+    headerModal.modal.classList.toggle('header-modal-is-hidden');
   }
 })();
+
+(() => {
+  const buyNowModal = {
+    openModalBtn: document.querySelector('[data-modal-open-buy-now]'),
+    closeModalBtn: document.querySelector('[data-modal-close-buy-now]'),
+    modal: document.querySelector('[data-modal-buy-now]'),
+  };
+
+  buyNowModal.openModalBtn.addEventListener('click', toggleModal);
+  buyNowModal.closeModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    buyNowModal.modal.classList.toggle('buy-now-modal-is-hidden');
+  }
+})();
+
+// Country prefixes for telephone numbers
