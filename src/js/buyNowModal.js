a686b97aeb3aa4f0b44071943e5f7f4c2bd7792b
robertSovar <133514926+robertSovar@users.noreply.github.com>
2024-02-17 18:58:51 +0200

    update top-sellers section

--- src/js/buyNowModal.js
+++ src/js/buyNowModal.js
@@ -1,17 +1,20 @@
 const buyNowModal = () => {
   const buyNowModal = {
     openModalBtn: document.querySelector('[data-modal-open-buy-now]'),
+    openSecModalBtn: document.querySelector('[data-modal-open-sec]'),
     closeModalBtn: document.querySelector('[data-modal-close-buy-now]'),
     modal: document.querySelector('[data-modal-buy-now]'),
   };
 
   buyNowModal.openModalBtn.addEventListener('click', toggleModal);
+  buyNowModal.openSecModalBtn.addEventListener('click', toggleModal);
   buyNowModal.closeModalBtn.addEventListener('click', toggleModal);
 
   document.addEventListener('click', function (event) {
     if (
       !buyNowModal.modal.contains(event.target) &&
-      !event.target.matches('[data-modal-open-buy-now]')
+      !event.target.matches('[data-modal-open-buy-now]') &&
+      !event.target.matches('[data-modal-open-sec]')
     ) {
       buyNowModal.modal.classList.add('buy-now-modal-is-hidden');
     }
